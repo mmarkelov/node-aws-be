241622a5f5db0564cb5f45a2212579d00c1ce1c5
mmarkelov <maks-markel@mail.ru>
2020-11-27 12:15:42 +0300

    Update serverless config to fix cors errors

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,85 +1,103 @@
-import type { Serverless } from 'serverless/aws';
+import type {Serverless} from 'serverless/aws';
 
 const serverlessConfiguration: Serverless = {
-  service: 'import-service',
-  frameworkVersion: '2',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
-    region: 'eu-west-1',
-    apiGateway: {
-      minimumCompressionSize: 1024,
+    service: 'import-service',
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true
+        }
     },
-    environment: {
-      BUCKET: 'node-aws-files',
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-    },
-    iamRoleStatements: [
-      {
-        Effect: 'Allow',
-        Action: 's3:ListBucket',
-        Resource: 'arn:aws:s3:::${self:provider.environment.BUCKET}'
-      },
-      {
-        Effect: 'Allow',
-        Action: 's3:*',
-        Resource: 'arn:aws:s3:::${self:provider.environment.BUCKET}/*'
-      },
-    ],
-  },
-  functions: {
-    importProductsFile: {
-      handler: 'handler.importProductsFile',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: '/',
-            request: {
-              parameters: {
-                querystrings: {
-                  name: true
-                }
-              }
+    // Add the serverless-webpack plugin
+    plugins: ['serverless-webpack'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs12.x',
+        region: 'eu-west-1',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+        },
+        environment: {
+            BUCKET: 'node-aws-files',
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        },
+        iamRoleStatements: [
+            {
+                Effect: 'Allow',
+                Action: 's3:ListBucket',
+                Resource: 'arn:aws:s3:::${self:provider.environment.BUCKET}'
             },
-            authorizer: {
-              name: 'basicAuthorizer',
-              arn: '${cf:authorization-service-${self:provider.stage}.BasicAuthorizerLambdaFunctionQualifiedArn}',
-              resultTtlInSeconds: 0,
-              identitySource: 'method.request.header.Authorization',
-              type: 'token'
-            }
-          }
-        }
-      ]
+            {
+                Effect: 'Allow',
+                Action: 's3:*',
+                Resource: 'arn:aws:s3:::${self:provider.environment.BUCKET}/*'
+            },
+        ],
     },
-    importFileParser: {
-      handler: 'handler.importFileParser',
-      events: [
-        {
-          s3: {
-            bucket: '${self:provider.environment.BUCKET}',
-            event: 's3:ObjectCreated:*',
-            rules: [
-              {
-                prefix: 'uploaded/',
-                suffix: ''
-              }
-            ],
-            existing: true
-          }
+    functions: {
+        importProductsFile: {
+            handler: 'handler.importProductsFile',
+            events: [
+                {
+                    http: {
+                        method: 'get',
+                        path: '/',
+                        request: {
+                            parameters: {
+                                querystrings: {
+                                    name: true
+                                }
+                            }
+                        },
+                        cors: true,
+                        authorizer: {
+                            name: 'basicAuthorizer',
+                            arn: '${cf:authorization-service-${self:provider.stage}.BasicAuthorizerLambdaFunctionQualifiedArn}',
+                            resultTtlInSeconds: 0,
+                            identitySource: 'method.request.header.Authorization',
+                            type: 'token'
+                        }
+                    }
+                }
+            ]
+        },
+        importFileParser: {
+            handler: 'handler.importFileParser',
+            events: [
+                {
+                    s3: {
+                        bucket: '${self:provider.environment.BUCKET}',
+                        event: 's3:ObjectCreated:*',
+                        rules: [
+                            {
+                                prefix: 'uploaded/',
+                                suffix: ''
+                            }
+                        ],
+                        existing: true
+                    }
+                }
+            ]
         }
-      ]
+    },
+    resources: {
+        Resources: {
+            GatewayResponseDefault4XX: {
+                Type: 'AWS::ApiGateway::GatewayResponse',
+                Properties: {
+                    ResponseParameters: {
+                        'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+                        'gatewayresponse.header.Access-Control-Allow-Methods': "'GET,OPTIONS'"
+                    },
+                    ResponseType: 'DEFAULT_4XX',
+                    RestApiId: {
+                        Ref: 'ApiGatewayRestApi'
+                    }
+                }
+            }
+        }
     }
-  }
 }
 
 module.exports = serverlessConfiguration;
