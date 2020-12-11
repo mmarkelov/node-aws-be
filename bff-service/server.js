8e8ff4f6f027651a0b5e849833db2251d272d596
mmarkelov <maks-markel@mail.ru>
2020-12-11 20:59:18 +0300

    Add simple cache

--- bff-service/server.js
+++ bff-service/server.js
@@ -4,6 +4,8 @@ const axios = require('axios').default;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cache = new Map();
+const trashhold = 120;
 
 app.use(express.json());
 
@@ -25,9 +27,27 @@ app.all('/*', (req, res) => {
 			...(Object.keys(req.body || {}).length > 0 && {data: req.body})
 		};
 
+		if (req.method === 'GET' && recipient === 'product') {
+			if (cache.has('products')) {
+				const [products, timestamp] = cache.get('products')
+				if ((Date.now() - timestamp) / 1000 <= trashhold) {
+					// Cache is still valid
+					res.json(products);
+				} else {
+					cache.delete('products')
+				}
+				return;
+			}
+		}
+
 		axios(axiosConfig)
 			.then(response => {
 				console.log('response', response.data);
+				if (req.method === 'GET' && recipient === 'product') {
+					if (!cache.has('products')) {
+						cache.set('products', [response.data, Date.now()]);
+					}
+				}
 				res.json(response.data);
 			})
 			.catch(error => {
